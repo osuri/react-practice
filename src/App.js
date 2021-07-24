99bbb91b1aa1abd2781254046895e231e302e9e5
osuri <lokanadham.o@hotmail.com>
2021-07-24 20:43:14 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch, NavLink, Redirect} from 'react-router-dom';
-import About from "./About";
-import Hidden from "./Hidden";
-import Home from "./Home";
-import NavBar from "./NavBar";
-import NotFound from "./NotFound";
-import PrivatePage from "./PrivatePage";
 
 
 import "./styles.css";
@@ -13,15 +6,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <PrivatePage path="/hidden" component={Hidden} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
+      <h1>Hello World</h1>
     </div>
   );
 }
--- src/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  
-  return <h3>Hello World. This is Home component</h3>;
-};
-export default Home;
